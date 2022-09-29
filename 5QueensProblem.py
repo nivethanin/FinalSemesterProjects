0f2b91c5241ea01c4a4173b825f4fb4423c5d3eb
nivethanin <nivethanin@gmail.com>
2022-09-29 08:54:05 -0400

    mmm

--- 5QueensProblem.py
+++ 5QueensProblem.py
@@ -1,70 +1,70 @@
+class Puzzle:
 
-b = [   [0, 0, 0, 0],
+    b = [   
+        [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ]
- 
-boardSize = len(b)
 
 
-
-def printBoard (board):
-    for i in range(boardSize):
-        for j in range(boardSize):
-            print(board[i][j], end = ' ')
-        
-        print()
-
-def checkLastRow(board):
-    for j in range(len(board)):
-        if board[len(board)][j] == 1:
-            return True
-        
-    return False
-
-def clearBoard(board):
-    for i in range(len(board)):
-        for j in range(len(board)):
-            board[i][j]=0
-
-
-def checkValid(board, row, col):
+    def isSafe(board, row, col):
     
-    # Check if row is valid
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+        # Check if row is valid
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
 
-    # Check if column is valid
-    for j in range(row):
-        if board[j][col] == 1:
-            return False
-    
-    # Check if left diagonal is valid
-    for i, j in zip(range(row,-1, -1), range(col,-1,-1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check if right diagonal (only left side) is valid
-    for i, j in zip(range(row, boardSize, 1), range(col,-1,-1)):
-        if board[i][j] == 1:
-            return False
-    
-    return True
-
-def solve(board):
-    for i in range(boardSize):
-        for j in range(boardSize):
-            if checkValid(board,i,j):
-                board[i][j] = 1
-                if i == boardSize:
-                    printBoard(board)
-                
-            else:
-                # clearBoard(board)
-                printBoard(board)
+        # Check if column is valid
+        for j in range(row):
+            if board[j][col] == 1:
+                return False
         
-        
+        # Check if left diagonal is valid
+        for i, j in zip(range(row,-1, -1), range(col,-1,-1)):
+            if board[i][j] == 1:
+                return False
 
-solve(b)
+        # Check if right diagonal (only left side) is valid
+        for i, j in zip(range(row, len(board), 1), range(col,-1,-1)):
+            if board[i][j] == 1:
+                return False
+        
+        return True
+
+
+    def placeQueen(board, i, j):
+        board[i][j]=1
+
+
+    def removeQueen(board, i, j):
+        board[i][j]=0
+
+
+    def displayBoard(board):
+        for i in range(len(board)):
+            for j in range(len(board)):
+                print(board[i][j], end = ' ')
+            print()
+
+
+    def findASafePlace(board, row):     
+        if row == len(board):
+            board.displayBoard(board)
+
+        for i in range(len(board)):
+            if board.isSafe(board, row, i):
+                board.placeQueen(board, row, i)
+
+                board.findASafePlace(board, row+1)
+
+                board.removeQueen(board, row, i)
+            
+
+
+N = input("What is the size of the board?")
+
+createdBoard=[[ [0] * N for i in range(N)]]
+solution = Puzzle()
+solution.displayBoard(createdBoard)
+
