bf25070522c6988dd562ee4f3080d28fc0aef8e3
DEVQA\niruthayanathan <nnathan@cbnco.com>
2022-10-26 13:56:57 -0400

    Oct-26

--- APrimeSearch.py
+++ APrimeSearch.py
@@ -83,10 +83,10 @@ class Graph:
     def h(self, n):
         return self.heuristicGraph[n]
  
-    def a_star_algorithm(self, start, stop):
+    def a_star_algorithm(self, start, goal):
         # In this open_lst is a lisy of nodes which have been visited, but who's 
         # neighbours haven't all been always inspected, It starts off with the start 
-  #node
+        #node
         # And closed_lst is a list of nodes which have been visited
         # and who's neighbors have been always inspected
         open_lst = set([start])
@@ -100,6 +100,8 @@ class Graph:
         # par contains an adjac mapping of all nodes
         par = {}
         par[start] = start
+
+        totalCost = []
  
         while len(open_lst) > 0:
             n = None
@@ -108,28 +110,38 @@ class Graph:
             for v in open_lst:
                 if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
                     n = v
+                    totalCost.append(poo[v])
+
  
             if n == None:
                 print('Path does not exist!')
                 return None
  
-            # if the current node is the stop
+            # if the current node is the goal
             # then we start again from start
-            if n == stop:
+            if n == goal:
                 reconst_path = []
  
                 while par[n] != n:
                     reconst_path.append(n)
                     n = par[n]
+                    print(n)
  
                 reconst_path.append(start)
- 
+
                 reconst_path.reverse()
  
                 print('Path found: {}'.format(reconst_path))
+                print("Total cost:" + str(totalCost))
+                tc = 0
+                for i in totalCost:  
+                    tc += i
+
+                print(tc)
                 return reconst_path
  
             # for all the neighbors of the current node do
+            print(self.get_neighbors(n).items())
             for m, weight in self.get_neighbors(n).items():
               # if the current node is not presentin both open_lst and closed_lst
                 # add it to open_lst and note n as it's par
@@ -137,7 +149,9 @@ class Graph:
                     open_lst.add(m)
                     par[m] = n
                     poo[m] = poo[n] + weight
- 
+                    
+
+
                 # otherwise, check if it's quicker to first visit n, then m
                 # and if it is, update par data and poo data
                 # and if the node was in the closed_lst, move it to open_lst
@@ -145,9 +159,10 @@ class Graph:
                     if poo[m] > poo[n] + weight:
                         poo[m] = poo[n] + weight
                         par[m] = n
- 
+                        totalCost.pop()
+
                         if m in closed_lst:
-                            closed_lst.remove(m)
+                            closed_lst.remove(m)                         
                             open_lst.add(m)
  
             # remove n from the open_lst, and add it to closed_lst
@@ -164,11 +179,8 @@ def main():
     heuristicLoader("input_heuristic(1).txt", heuristicGraph)
 
     g = Graph(mainGraph, heuristicGraph)
+    g.a_star_algorithm("Craiova", "Bucharest")
 
-    g.a_star_algorithm("Craiova", "Hirsova3")
-    
-
-    
 
 if __name__ == "__main__":
     main()
