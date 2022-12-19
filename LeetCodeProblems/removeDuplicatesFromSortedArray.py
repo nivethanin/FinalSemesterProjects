295ad2d7ed9505fe1400a46d2e70185c64d84243
DEVQA\niruthayanathan <nnathan@cbnco.com>
2022-12-19 09:41:47 -0500

    .

--- LeetCodeProblems/removeDuplicatesFromSortedArray.py
+++ LeetCodeProblems/removeDuplicatesFromSortedArray.py
@@ -1,28 +1,25 @@
 class Solution:
 
-    # def removeDuplicates(self, nums:list[int]) -> int:
-    #     temp = -101
-
-    #     i=0
-    #     while nums[i]:
-    #         print(i)
-    #         if nums[i] == temp:
-    #             nums.pop(i)
-    #         else:
-    #             temp = nums[i]
-            
-    #         i+=1
-    
-    #     return len(nums), nums
-
     def removeDuplicates(self, nums:list[int]) -> int:
-        nums = set(nums)
-        return len(nums), nums
+        if not nums:
+            return 0
+        
+        temp = 0
 
-    
+        for i in range(1, len(nums)):
+            print(nums)
+            if nums[temp] != nums[i]:
+                temp +=1
+                nums[temp] = nums[i]
+            
+            nums[i] = '_'
+        
+        return temp + 1
+
+
 
 arr = [0,0,1,1,2,3,4,4,4,4,5]
 
 g = Solution()
-print(g.removeDuplicates(arr[:]))
+print(g.removeDuplicates(arr))
 print(f"The final array: {arr}")
