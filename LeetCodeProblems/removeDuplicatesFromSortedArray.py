5e673cd9af1542a6ac1df03056b7818ebcfddfdd
DEVQA\niruthayanathan <nnathan@cbnco.com>
2022-12-19 09:45:05 -0500

    .

--- LeetCodeProblems/removeDuplicatesFromSortedArray.py
+++ LeetCodeProblems/removeDuplicatesFromSortedArray.py
@@ -5,10 +5,13 @@ class Solution:
             return 0
         
         temp = 0
+        #Temp position will be the lagging pointer.
 
         for i in range(1, len(nums)):
             print(nums)
             if nums[temp] != nums[i]:
+                #If they're the not the same then shift it over to the temp position
+                #temp pos only updates when a new valid number is introduced
                 temp +=1
                 nums[temp] = nums[i]
             
