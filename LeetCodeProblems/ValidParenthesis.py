e32e819f039422d2c86f75e196423dfda02e87eb
DEVQA\niruthayanathan <nnathan@cbnco.com>
2023-01-13 17:00:38 -0500

    .

--- LeetCodeProblems/ValidParenthesis.py
+++ LeetCodeProblems/ValidParenthesis.py
@@ -15,8 +15,18 @@ class Solution:
         """Could add case for starting with open brackets"""
 
         for i in range(len(fs)):
-            if len(stack)<1:
-                stack.append(fs)
+            if len(stack)<1 or fs[i] not in self.DIC:
+                stack.append(fs[i])
+
+            elif fs[i] in self.DIC and stack[len(stack)-1] == fs[i]:
+                stack.pop()
+            
+            else:
+                return False
+
+        
+        if stack
+
             
         
             
